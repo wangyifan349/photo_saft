8e433064e450013488fa4e603c43e9067ef91e80
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-05-03 07:03:54 +0000

    Update 图片隐私保护_GUI.py

--- 图片隐私保护_GUI.py
+++ 图片隐私保护_GUI.py
@@ -1,28 +1,14 @@
-import os,random,time
-import piexif
+#!/usr/bin/python3
+#pip install piexif
+#pip install pyexif
+#pip install pillow
+import os,random,time,piexif,threading
 from PIL import Image
 from tkinter import Tk, filedialog, Label, Button, StringVar, Entry, Frame
 from tkinter import messagebox
-
-
-def get_file_path(file_name):#根据操作系统类型获取文件路径
-    import platform# 导入platform模块，获取当前操作系统类型
-    # 获取操作系统类型
-    os_type = platform.system()    # 根据操作系统类型，拼接文件路径
-    if os_type == "Windows":
-        file_path = ".\\" + file_name.replace("/", "\\")
-    elif os_type == "Linux":
-        file_path = "./" + file_name.replace("\\", "/")
-    else:
-        raise OSError("未知的操作系统类型")# 如果是未知的操作系统类型，则抛出异常
-    return os_type, file_path# 返回文件路径和操作系统类型
-
-
-
-
+from datetime import datetime, timedelta
 print("本程序可以擦除照片有关相机参数、GPS、曝光、白平均等信息，保护用户隐私。")
-#print("本程序在github已开源.如有考虑不全等问题，欢迎告知wangyifan349@gmail.com")
-# 你的 forge_exif_data 函数和 is_directory_exist 函数保持不变
+print("同时写入一定的GPS、苹果手机信息以混淆")
 def forge_exif_data(image_path):
     try:
         with Image.open(image_path) as img:
@@ -60,15 +46,40 @@ def forge_exif_data(image_path):
 
 #这里面是伪造GPS信息的。
 ###################################
+            altitude = random.randint(5, 100)  # 随机生成高度，单位为米
 
-            fake_exif_data = {
-                "0th":{
+            # 随机生成高度，单位为米
+            # 随机生成时间，向前回退 1-50 天，时分秒随机生成
+            days = random.randint(1, 50)
+            hours = random.randint(0, 23)
+            minutes = random.randint(0, 59)
+            seconds = random.randint(0, 59)
+            delta = timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+            timestamp = datetime.now() - delta
+            print(timestamp)#它的构造函数可以接受四个可选参数：days、hours、minutes 和 seconds，用于表示时间间隔的天、小时、分钟和秒数。
+            # 将时间转换为字符串格式
+            date_string = timestamp.strftime("%Y:%m:%d %H:%M:%S")
+
+# 创建 GPS 信息字典
+            gps_ifd = {
+                piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),  # GPS 版本信息
+                piexif.GPSIFD.GPSLatitudeRef: "N",  # 纬度参考（N表示北纬，S表示南纬）
+                piexif.GPSIFD.GPSLongitudeRef: "E",  # 经度参考（E表示东经，W表示西经）
+                piexif.GPSIFD.GPSLatitude: latitude_new,
+                piexif.GPSIFD.GPSLongitude: longitude_new,
+                piexif.GPSIFD.GPSDateStamp: date_string,  # GPS 日期时间戳
+                piexif.GPSIFD.GPSAltitudeRef: 1,  # GPS 海拔高度参考（1 表示海平面以上）
+                piexif.GPSIFD.GPSAltitude: (altitude, 1)  # GPS 海拔高度
+            }
+            ZeroTH={
         piexif.ImageIFD.Make: "Apple",  # 相机制造商
         piexif.ImageIFD.Model: "iPhone 13 Pro Max",  # 相机型号
         piexif.ImageIFD.Software: "iOS 15",  # 软件版本
         piexif.ImageIFD.Orientation: 1,  # 图像方向（1表示正常方向）
-        },
-        "Exif": {
+        piexif.ImageIFD.Software: "IOS 15",#用于记录图像处理软件的名称和版本信息，如果图像是由 Apple 设备生成的，则通常会将此字段设置为 "iOS xxx" 或 "macOS xxx"，其中 "xxx" 是操作系统的版本号。例如，如果图像是由运行 iOS 15 的 iPhone 13 Pro Max 拍摄的，则 piexif.ImageIFD.Software 可以设置为 "iOS 15"。
+        
+        }
+            exif_info= {
         piexif.ExifIFD.ExposureTime: (1, 50),  # 曝光时间 1/50s
         piexif.ExifIFD.FNumber: (28, 10),  # F值 f/2.8
         piexif.ExifIFD.ISOSpeedRatings: 100,  # ISO速度
@@ -94,14 +105,11 @@ def forge_exif_data(image_path):
         piexif.ExifIFD.ExposureProgram: 2,  # 曝光程序（2表示正常程序）
         piexif.ExifIFD.ExposureMode: 0,  # 曝光模式（0表示自动曝光，1表示手动曝光）
         piexif.ExifIFD.SceneCaptureType: 0,  # 场景拍摄类型（0表示标准，1表示风景，2表示人像，3表示夜景等）
-        },
-       "GPS": {
-        piexif.GPSIFD.GPSLatitudeRef: "N",  # 纬度参考（N表示北纬，S表示南纬）
-        piexif.GPSIFD.GPSLongitudeRef: "E",  # 经度参考（E表示东经，W表示西经）
-        piexif.GPSIFD.GPSLatitude: latitude_new ,
-        piexif.GPSIFD.GPSLongitude: longitude_new
-       
-    }
+        }
+            fake_exif_data = {
+                "0th":ZeroTH,
+                "Exif":exif_info ,
+                "GPS": gps_ifd
                 }
             # 将伪造的Exif数据写入图片
             print("开始写入伪造数据")
@@ -129,25 +137,35 @@ def browse_directory():
     # 将所选目录的路径插入到条目(widget)中
     directory_entry.insert(0, image_directory)
 
-def process_images():
-    if is_directory_exist(image_directory):
-        for filename in os.listdir(image_directory):
-            if os.path.splitext(filename)[1].lower() in ('.webp','.jpg', '.jpeg', '.bmp', '.gif', '.png'):#你可以添加更多的处理的格式('.webp', '.jpg', '.jpeg', '.bmp', '.gif', '.png', '.tiff', '.ico', '.svg', '.eps')
-                image_path = os.path.join(image_directory, filename)
+def process_images_recursively(directory):
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if os.path.splitext(filename)[1].lower() in ('.webp', '.jpg', '.jpeg', '.bmp', '.gif', '.png', '.PNG', '.WEBP', '.JPG', '.JPEG', '.BMP', '.GIF'):
+                image_path = os.path.join(root, filename)
                 print(f"正在处理 {image_path}...")
                 forge_exif_data(image_path)
-
+def process_images_thread():
+    # 使用线程处理图像，以防止GUI阻塞
+    process_thread = threading.Thread(target=process_images)
+    process_thread.start()
+    
+def process_images():
+    if is_directory_exist(image_directory):
+        process_images_recursively(image_directory)
         status_label.config(text="处理完成")
-        messagebox.showinfo("感谢使用", "已经清除了图片中的隐私信息，同时写入了一定的伪造信息")  # 添加这一行来显示弹窗
+        messagebox.showinfo("感谢使用", "已经清除了图片中的隐私信息，同时写入了一定的伪造信息")
     else:
         status_label.config(text="路径无法识别，有任何问题，请联系开发者。")
-
 # 创建GUI窗口
 root = Tk()
 root.title("Exif数据伪造工具")
 root.geometry("400x200")
-
-image_directory = ""  # 在这里添加初始值
+# 获取当前用户的用户名
+user_name = os.getlogin()
+# 获取图片目录的路径
+image_directory = os.path.join("C:\\", "Users", user_name, "Pictures")
+print(image_directory)
+#image_directory = ""  # 在这里添加初始值
 
 # 添加目录选择和显示组件
 directory_label = Label(root, text="请选择图片文件夹：", wraplength=350)
@@ -160,7 +178,7 @@ directory_entry = Entry(root, width=50)
 directory_entry.pack(pady=5)
 
 # 添加开始处理按钮
-process_button = Button(root, text="开始处理", command=process_images)
+process_button = Button(root, text="开始处理", command=process_images_thread)
 process_button.pack(pady=10)
 
 # 添加状态标签
